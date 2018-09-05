ce888906a05652f003a36b4a2e318405224b965e
dennistikhomirov <dennis00010011b@gmail.com>
2018-09-06 02:58:54 +0800

    adapt e2e to firefox, revert selenium to v3.6.0

--- test/e2e/func.js
+++ test/e2e/func.js
@@ -16,7 +16,7 @@ module.exports = {
   getExtensionIdChrome,
   getExtensionIdFirefox,
   clearField,
-  }
+}
 
 function delay (time) {
   return new Promise(resolve => setTimeout(resolve, time))
@@ -61,7 +61,7 @@ async function installWebExt (driver, extension) {
   await driver.getExecutor()
     .defineCommand(cmd.getName(), 'POST', '/session/:sessionId/moz/addon/install')
 
-  return await driver.execute(cmd, 'installWebExt(' + extension + ')')
+  return await driver.schedule(cmd, 'installWebExt(' + extension + ')')
 }
 
 async function clearField (field, number) {
