8a313f69f48f52cbdd6c9d85e972644938ecef9d
dennistikhomirov <dennis00010011b@gmail.com>
2018-09-06 04:54:05 +0800

    4

--- test/e2e/metamask.spec.js
+++ test/e2e/metamask.spec.js
@@ -187,6 +187,8 @@ describe('Metamask popup page', async function () {
         await delay(700)
         await button.click()
         const title = await waitUntilShowUp(screens.changePassword.title)
+        console.log(title)
+        console.log('text'+(await title.getText())
         assert.equal(await title.getText(), screens.changePassword.titleText, '"Change password" screen contains incorrect title')
       })
 
