df75137ff6fd437c47df46c10f1f3e46e95184dd
dennistikhomirov <dennis00010011b@gmail.com>
2018-12-30 10:11:38 -0800

    e2e:test stability#8

--- test/e2e/metamask.spec.js
+++ test/e2e/metamask.spec.js
@@ -13,7 +13,7 @@ const eventsEmitter = 'https://vbaranov.github.io/event-listener-dapp/'
 
 describe('Metamask popup page', async function () {
 
-  this.timeout(20000)
+  this.timeout(15*60*1000)
 
   const f = new Func()
   let driver, tokenAddress, extensionId
